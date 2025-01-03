d56eaad44234aa41953884e00118346b7e08c491
Ingryd <ingrydcduarte@gmail.com>
2025-01-03 01:12:12 -0300

    fix: lights + feat: abajur basic

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const aspect = w / h; //Tamanho?
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.x = 1.5;
+camera.position.x = 0;
 camera.position.y = 0.4;
 camera.position.z = 3;
 
@@ -32,62 +32,48 @@ controls.maxPolarAngle = Math.PI / 2;
     camera.position.x = Math.max(1, Math.min(2.6, camera.position.x));
     camera.position.y = Math.max(0.4, Math.min(2, camera.position.x));
     camera.position.z = Math.max(2, Math.min(4, camera.position.z));
-  }); */
-
+  });
+ */
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x400987);
+scene.background = new THREE.Color(0x121212);
 
-// Luz
-const ambientLight = new THREE.AmbientLight(0x606060, 0.5); // Luz suave de fundo
-scene.add(ambientLight);
+// MARK: Luz
 
 // Luz Direcional (habilitar sombras)
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-directionalLight.position.set(-3, 5, 3);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.width = 1000;
-directionalLight.shadow.mapSize.height = 1000;
-directionalLight.shadow.camera.near = 0.1;
-directionalLight.shadow.camera.far = 50;
-directionalLight.shadow.camera.left = 0;  // Configuração para ajustar o tamanho da sombra
-directionalLight.shadow.camera.right = 5;
-directionalLight.shadow.camera.top = 5;
-directionalLight.shadow.camera.bottom = -5;
-scene.add(directionalLight);
 
 // Luz de Spot com sombras
-const spotLight = new THREE.SpotLight(0xffaa33, 2);
-spotLight.position.set(0, 5, 2);
+const spotLight = new THREE.SpotLight(0xba204e, 1);
+spotLight.position.set(3, 4, 0.7);
 spotLight.angle = Math.PI / 6;
-spotLight.penumbra = 0.3;
+spotLight.penumbra = 1.0;
 spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 1000;
-spotLight.shadow.mapSize.height = 1000;
+spotLight.shadow.mapSize.width = 200;
+spotLight.shadow.mapSize.height = 200;
 scene.add(spotLight);
 
+const spot2 = new THREE.SpotLight(0xffffff, 0.3);
+spot2.position.set(0, 5, 0.7);
+spot2.angle = Math.PI / 8;
+spot2.penumbra = 0.2;
+spot2.castShadow = true;
+spot2.shadow.mapSize.width = 1000;
+spot2.shadow.mapSize.height = 1000;
+scene.add(spot2);
+
 // Luz de preenchimento suave (não gera sombra, apenas luz ambiente)
-const pointLight = new THREE.PointLight(0x33aaff, 0.5, 10);
-pointLight.position.set(1, 2, 1);
+const pointLight = new THREE.PointLight(0xb51246, 0.5, 5);
+pointLight.position.set(0, 2, 1);
+pointLight.penumbra =1;
 pointLight.castShadow = true;
 scene.add(pointLight);
 // Luz Hemisphere ajustada para mais contraste
 const hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000022, 0.5); // Céu e chão
 scene.add(hemisphereLight);
 
-// Animação para alterar dinamicamente as cores das luzes
-let hue = 0;
-function animateLights() {
-    hue += 0.01;
-    const color = new THREE.Color(`hsl(${(hue * 360) % 360}, 50%, 50%)`);
-    spotLight.color = color;
-    pointLight.color = color;
-    requestAnimationFrame(animateLights);
-}
-animateLights();
 
 // Monitor
-const monitorGeometry = new THREE.BoxGeometry(3, 2, 0.1);
-const monitorMaterial = new THREE.MeshStandardMaterial({ color: 0x444444 });
+const monitorGeometry = new THREE.BoxGeometry(4, 2, 0.1);
+const monitorMaterial = new THREE.MeshStandardMaterial({ color: 0x151515 });
 const monitor = new THREE.Mesh(monitorGeometry, monitorMaterial);
 monitor.position.set(0, 1.5, -0.5);
 monitor.castShadow = true;
@@ -96,7 +82,7 @@ scene.add(monitor);
 
 const sup = new THREE.BoxGeometry(1, 0.2, 0.2)
 const sup2= new THREE.BoxGeometry(1, 0.2, 0.2)
-const supMat = new THREE.MeshBasicMaterial({color: 0x444444})
+const supMat = new THREE.MeshStandardMaterial({color: 0x151515})
 const suporte1 = new THREE.Mesh(sup, supMat);
 const suporte2 = new THREE.Mesh(sup2, supMat);
 suporte2.castShadow = true;
@@ -110,12 +96,12 @@ suporte1.receiveShadow = true;
 scene.add(suporte1);
 scene.add(suporte2);
 
-
 //Mesa
 const loader = new THREE.TextureLoader();
-const deckGeometry = new THREE.BoxGeometry(4, 0.2, 3);
-const deckMaterial = new THREE.MeshBasicMaterial({ map: loader.load('madeira.png') })
-const deck = new THREE.Mesh(deckGeometry, deckMaterial);
+const deckGeometry = new THREE.BoxGeometry(10, 0.2, 3);
+const deckMaterial = new THREE.MeshPhongMaterial({color: 0xfafafa})
+/* const deckMaterial = new THREE.MeshPhongMaterial({ map: loader.load('madeira.png') })
+ */const deck = new THREE.Mesh(deckGeometry, deckMaterial);
 deck.position.set(0, -0.2, 0.6);
 deck.castShadow = true;
 deck.receiveShadow = true;
@@ -132,7 +118,7 @@ video.crossOrigin = 'anonymous';
 
 video.oncanplaythrough = function () {
     const videoTexture = new THREE.VideoTexture(video);
-    const screenGeometry = new THREE.PlaneGeometry(2.5, 1.5);
+    const screenGeometry = new THREE.PlaneGeometry(3.8, 1.7);
     const screenMaterial = new THREE.MeshBasicMaterial({ map: videoTexture });
     const screen = new THREE.Mesh(screenGeometry, screenMaterial);
     screen.position.set(0, 1.5, -0.4);
@@ -144,12 +130,32 @@ video.oncanplaythrough = function () {
 
 // Base do Computador
 const baseGeometry = new THREE.BoxGeometry(0.5, 1, 0.1);
-const baseMaterial = new THREE.MeshStandardMaterial({ color: 0x444444 });
+const baseMaterial = new THREE.MeshStandardMaterial({ color: 0x151515 });
 const base = new THREE.Mesh(baseGeometry, baseMaterial);
-base.position.set(0, 0, -0.5);
+base.position.set(0, 0.2, -0.5);
 base.castShadow = true;
 base.receiveShadow = true;
 scene.add(base);
+
+//Suporte Abajur
+const luzAbajur = new THREE.PointLight(0xd6ad1a, 4, 3);
+luzAbajur.position.set(-4, 2.5, 0);
+luzAbajur.penumbra =1;
+luzAbajur.castShadow = true;
+scene.add(luzAbajur);
+
+const geometry = new THREE.CylinderGeometry(0.1, 0.1, 3, 32, 3); 
+const circleGeo = new THREE.CylinderGeometry(0.3, 1, 1, 32, 3, 1, false); 
+const material = new THREE.MeshPhongMaterial( {color: 0x7d3a10} ); 
+const mat = new THREE.MeshLambertMaterial( {color: 0x7d3a10} ); 
+const cylinder = new THREE.Mesh( geometry, material );
+const circleCyl = new THREE.Mesh(circleGeo, mat);
+
+circleCyl.position.set(-4,2.5,0);
+cylinder.position.set(-4, 1, 0);
+scene.add(cylinder);
+scene.add(circleCyl);
+
 
 // MARK: Texto 2D
 const textDiv = document.createElement('div');
@@ -183,14 +189,18 @@ document.body.appendChild(textDiv);
 
 // Tecla + Teclado
 const keyGeometry = new THREE.BoxGeometry(1, 0.1, 0.2);
-const keyMaterial = new THREE.MeshStandardMaterial({ color: 0x161616 });
+const keyGeo = new THREE.BoxGeometry(0.2, 0.1, 0.2);
+
+const keyMaterial = new THREE.MeshPhongMaterial({ color: 0x171717 });
 const key = new THREE.Mesh(keyGeometry, keyMaterial);
+const keyG = new THREE.Mesh(keyGeo, keyMaterial);
+keyG.position.set(-1, 0.1, 1.8);
 key.position.set(0, 0.1, 1.8);
 scene.add(key);
-
+scene.add(keyG);
 
 const keyboardGeometry = new THREE.BoxGeometry(3, 0.2, 1);
-const keyboardMaterial = new THREE.MeshStandardMaterial({ color: 0x101010 });
+const keyboardMaterial = new THREE.MeshStandardMaterial({ color: 0x404040 });
 const keyboard = new THREE.Mesh(keyboardGeometry, keyboardMaterial);
 keyboard.position.set(0, 0, 1.5);
 scene.add(keyboard);
